34ba43d13b0d5257dd002544dd24593c920d5c63
Mykhail Bobrykov <bobrykovmykhail@gmail.com>
2021-08-22 17:41:44 +0300

    add module display

---
+++ src/js/lib/modules/display.js
@@ -0,0 +1,29 @@
+import $ from '../core';
+
+//show elements
+$.prototype.show = function(){
+   for(let i = 0; i < this.length; i++){
+      if(!this[i].style) continue;
+       this[i].style.display = '';
+   }
+   return this;
+}
+
+//hide elements
+$.prototype.hide = function(){
+   console.log(this)
+   for(let i = 0; i < this.length; i++){
+      if(!this[i].style) continue;
+      this[i].style.display = 'none';
+   }
+   return this;
+}
+
+//toggle elements
+$.prototype.toggle = function(){
+   for(let i = 0; i < this.length; i++){
+      if(!this[i].style) continue;
+      this[i].style.display = this[i].style.display === 'none' ? '' : 'none';
+   }
+   return this;
+}
